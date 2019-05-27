2e0cea59e1d0455a3af73b18ae075ee2ade80cda
Kellen <khaynes31@gatech.edu>
2019-05-26 18:24:22 -0700

    changes

--- Stratego.py
+++ Stratego.py
@@ -2,14 +2,14 @@ from graphics import *
 
 
 class Tile:
-	def	__init__(self, position: Point, length: int, window, tile_type: str = "reg", colour = "brown"):
+	def	__init__(self, position: Point, length: int, window, tile_type: str = "reg"):
 
 		self.position = position
 		self.type = tile_type
 		self.rectangle = Rectangle(Point(0,0), Point(0,0))
 		self.length = length
 		self.window = window
-		self.colour = colour
+		self.colour = "blue" if self.type == "water" else "green"
 
 	def draw_square(self):
 		top_left_x = self.position.x * self.length
@@ -33,7 +33,7 @@ def drawGrid():
 		for j in range(size_of_grid):
 			#square = Rectangle(Point(i*length, j*length), Point(i*length + length, j*length+length))
 			#square.draw(win)
-			g = Tile( Point(i,j), length, win, tile_type = "water" if ((j == 4 or j == 5) and (i == 2 or i == 3 or i == 6 or i == 7)) else "reg", colour = "blue" if ((j == 4 or j == 5) and (i == 2 or i == 3 or i == 6 or i == 7)) else "green")
+			g = Tile( Point(i,j), length, win, tile_type = "water" if ((j == 4 or j == 5) and (i == 2 or i == 3 or i == 6 or i == 7)) else "reg")
 			g.draw_square()
 
 drawGrid()			
