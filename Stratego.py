4ba6ce163e975af138cee7309c5297f6ccee802b
Kellen <khaynes31@gatech.edu>
2019-05-26 18:02:17 -0700

    adding colours

--- Stratego.py
+++ Stratego.py
@@ -1,15 +1,40 @@
 from graphics import *
+
+
+class Tile:
+	def	__init__(self, position: Point, length: int, window, tile_type: str = "reg", colour = "brown"):
+
+		self.position = position
+		self.type = tile_type
+		self.rectangle = Rectangle(Point(0,0), Point(0,0))
+		self.length = length
+		self.window = window
+		self.colour = colour
+
+	def draw_square(self):
+		top_left_x = self.position.x * self.length
+		top_left_y = self.position.y * self.length
+		self.rectangle = Rectangle(Point(top_left_x , top_left_y ), Point(top_left_x + self.length, top_left_y + self.length))
+		self.rectangle.setFill(self.colour)
+		self.rectangle.draw(self.window)
+
+
+
+
 sides = 500
 win = GraphWin("Stratego", sides, sides)
-"""square = Rectangle(Point(0,0),Point(25,25))
-square.draw(win)
-win.getMouse()"""
 
 
 size_of_grid = 10
 length = int(sides/size_of_grid)
-for i in range(size_of_grid):
-	for j in range(size_of_grid):
-		square = Rectangle(Point(i*length, j*length), Point(i*length + length, j*length+length))
-		square.draw(win)
-win.getMouse()
+
+def drawGrid():
+	for i in range(size_of_grid):
+		for j in range(size_of_grid):
+			#square = Rectangle(Point(i*length, j*length), Point(i*length + length, j*length+length))
+			#square.draw(win)
+			g = Tile( Point(i,j), length, win, tile_type = "reg", colour = "blue" if ((j == 4 or j == 5) and (i == 2 or i == 3 or i == 6 or i == 7)) else "green")
+			g.draw_square()
+
+drawGrid()			
+win.getMouse() 	 	
